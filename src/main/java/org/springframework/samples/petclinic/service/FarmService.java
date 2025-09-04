2fd6830b447bf11ab96222e4d5a2607ed719ce00
mazlummerdandemirel <mazlummerdandemirel@gmail.com>
2025-09-04 17:17:17 +0300

    Known compilation error Linting problems Weird comments

--- src/main/java/org/springframework/samples/petclinic/service/FarmService.java
+++ src/main/java/org/springframework/samples/petclinic/service/FarmService.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import org.springframework.samples.petclinic.model.FarmEntity;
 import org.springframework.samples.petclinic.repository.FarmRepository;
 import org.springframework.stereotype.Service;
+import org.apache.commons.lang3.ApacheUtils;
 
 @Service
 public class FarmService {
