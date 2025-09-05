a0d531bb4c9d5b74e6c390ef018c447532e5c54b
mazlummerdandemirel <mazlummerdandemirel@gmail.com>
2025-09-05 09:17:09 +0300

    A comprehensive logic with multiple branches Missing access modifier

---
+++ src/main/java/org/springframework/samples/petclinic/utils/FarmUtility.java
@@ -0,0 +1,39 @@
+package org.springframework.samples.petclinic.utils;
+
+import static org.springframework.samples.petclinic.utils.FarmUtility.Options.DoAnotherThing;
+import static org.springframework.samples.petclinic.utils.FarmUtility.Options.DoEverything;
+import static org.springframework.samples.petclinic.utils.FarmUtility.Options.DoSomething;
+
+public class FarmUtility {
+
+	enum Options {
+		DoSomething,
+		DoEverything,
+		DoAnotherThing,
+	}
+
+	static void main(String[] args) {
+		Options choice = Options.valueOf(args[0]);
+		Options choice1 = Options.valueOf(args[1]);
+		if (choice.equals(DoEverything)) {
+			doSomething();
+			doAnother();
+		}
+		if (choice.equals(DoSomething)) {
+			doSomething();
+			if (choice1.equals(DoAnotherThing)) {
+				doAnother();
+			}
+		} else if (choice.equals(DoAnotherThing)) {
+			doAnother();
+		}
+	}
+
+	public static void doSomething() {
+		throw new RuntimeException();
+	}
+
+	public static void doAnother() {
+		throw new RuntimeException();
+	}
+}
